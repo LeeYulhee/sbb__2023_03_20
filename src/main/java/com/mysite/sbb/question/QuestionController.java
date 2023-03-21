3263b24f4fe15ed29a8bd00a52c6588964f2639a
LeeYulhee <reasonoflife39@gmail.com>
2023-03-21 10:23:45 +0900

    서비스

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -12,11 +12,11 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionsList = this.questionRepository.findAll();
+        List<Question> questionsList = this.questionService.getList();
         model.addAttribute("questionList", questionsList);
         return "question_list";
     }
